84f142517629998ddeb7755b8bcd118d969fd11e
spec <77427029+specowos@users.noreply.github.com>
2021-12-17 00:04:00 -0600

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -1,2 +1,2 @@
 --// Enjoy! Join the discord for info on how to use https://discord.gg/eTTjPKBdfT
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Sylixe/Scripts/main/MiztReanimate.lua',true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/UI/main.lua',true))()
