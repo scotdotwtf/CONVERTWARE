c8b328a434f4d222723aa6a3a0292fce5cbf6eab
spec <77427029+specowos@users.noreply.github.com>
2021-12-16 20:04:34 -0600

    Create Mizt Flings.lua

---
+++ convertware/Reanimation/Mizt Flings.lua
@@ -0,0 +1,15 @@
+_G.MiztReanimSettings = {
+    PermanentDeath = false,
+    NoHeadPermanentDeath = false,
+    Noclip = true,
+    HatReanimate = false,
+    SemiHatReanimate = false,
+    UseMizaruTorso = false,
+    FlingActive = true,
+    AlignMethod = true,
+    Netless = true,
+    ActiveLegacyNet = false,
+    NetlessVelocity = {0,0,-50}
+}
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Sylixe/Scripts/main/MiztReanimate.lua',true))()
