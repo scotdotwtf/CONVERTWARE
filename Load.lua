06130c75d8144cc411bd57a7853b062a9f1ace79
spec <77427029+specowos@users.noreply.github.com>
2021-12-16 23:58:54 -0600

    Create Load.lua

---
+++ Load.lua
@@ -0,0 +1,2 @@
+--// Enjoy! Join the discord for info on how to use https://discord.gg/eTTjPKBdfT
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Sylixe/Scripts/main/MiztReanimate.lua',true))()
