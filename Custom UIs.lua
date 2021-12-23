ab995611275dc2a6917c3d783a33408d35a58afc
spec <77427029+specowos@users.noreply.github.com>
2021-12-22 21:20:31 -0600

    Create Custom UIs.lua

---
+++ Custom UIs.lua
@@ -0,0 +1,10 @@
+--// just pick a loadstring that u want //--
+
+--// Krnl X UI
+loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/UI/main.lua',true))()
+
+--// Homebrew V4 UI
+-- coming soon
+
+--// Filterware
+-- coming soon
