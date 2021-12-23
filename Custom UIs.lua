3e91c3956cb02e363eeeff270c9098a9c2ed86b1
spec <77427029+specowos@users.noreply.github.com>
2021-12-22 21:20:49 -0600

    Update Custom UIs.lua

--- Custom UIs.lua
+++ Custom UIs.lua
@@ -1,7 +1,7 @@
 --// just pick a loadstring that u want //--
 
 --// Krnl X UI
-loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/UI/main.lua',true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/UI/krnl%20x.lua',true))()
 
 --// Homebrew V4 UI
 -- coming soon
