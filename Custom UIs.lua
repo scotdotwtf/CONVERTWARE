8c99c0e79e0f351fc9122f2a225c4f8e1f897004
spec <77427029+specowos@users.noreply.github.com>
2021-12-24 15:28:50 -0600

    Update Custom UIs.lua

--- Custom UIs.lua
+++ Custom UIs.lua
@@ -7,4 +7,5 @@ loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/
 loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/UI/homebrew%20ui.lua',true))()
 
 --// Filterware
--- coming soon
+loadstring(game:HttpGet('https://github.com/specowos/CONVERTWARE/blob/main/UI/filterware%20ui.lua',true))()
+
