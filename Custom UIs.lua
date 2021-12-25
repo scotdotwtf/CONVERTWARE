13afabb0a88dec7b0e695e1c0dd5f65cbeb4a60e
spec <77427029+specowos@users.noreply.github.com>
2021-12-24 18:44:33 -0600

    Update Custom UIs.lua

--- Custom UIs.lua
+++ Custom UIs.lua
@@ -7,7 +7,7 @@ loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/
 loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/UI/homebrew%20ui.lua',true))()
 
 --// Filterware V1 CMD Bar Remake
-loadstring(game:HttpGet('https://github.com/specowos/CONVERTWARE/blob/main/UI/filterware%20ui.lua',true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/UI/filterware%20ui.lua',true))()
 
 --// Filterware V2 UI Remake
 -- coming soon
