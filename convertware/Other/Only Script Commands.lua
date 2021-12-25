74b3aa00f1bc593d636f8f0c9419ea5c9e2bedb6
spec <77427029+specowos@users.noreply.github.com>
2021-12-24 22:09:35 -0600

    Update Only Script Commands.lua

--- convertware/Other/Only Script Commands.lua
+++ convertware/Other/Only Script Commands.lua
@@ -9,134 +9,134 @@ local function CMSG(text)
 end
 
 scriptcmds = {
-    ["sun sword"] =  function()
+    ["sun sword"] = [[
         CMSG(" > Running Yellow and Black Strong Sun Sword by ???")  
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Yellow%20and%20Black%20Strong%20Sun%20Sword%20Convert.lua',true))()
-    end,
+    ]],
 
-    ["huntsman"] =  function()
+    ["huntsman"] = [[
         CMSG(" > Running Phosphoria Huntsman Knife by ???")  
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Phosphoria%20Huntsman%20Knife%20Convert.lua',true))()
-    end,
+    ]],
     
-    ["kitchen"] =  function()
+    ["kitchen"] = [[
         CMSG(" > Running Kitchen Gun by WafflesAreVeryGood")  
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Kitchen%20Gun%20Convert.lua',true))()
-    end,
+    ]],
     
-    ["nep v"] =  function()
+    ["nep v"] = [[
         CMSG(" > Running Neptune/Neptunian V by NoobyGames12")
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Neptunian%20V%20Convert.lua',true))()
-    end,
+    ]],
     
-    ["xester"] =  function()
+    ["xester"] = [[
         CMSG(" > Running Xester V by Supr14")
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Xester%20Convert.lua',true))()
-    end,
+    ]],
     
-    ["krystal v1"] =  function()
+    ["krystal v1"] = [[
         CMSG(" > Running Krystal Dance v1 by The Krystal Team")
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Krystal%20Dance%20V1%20Convert.lua',true))()
-    end,
+    ]],
     
-    ["krystal v2"] =  function()
+    ["krystal v2"] = [[
         CMSG(" > Running Krystal Dance V2 by Supr14")
         CMSG(" [also credits to krystalteam for krystal dances v1]")
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Krystal%20Dances%20V2%20Convert.lua',true))()
-    end,
+    ]],
     
-    ["abyss"] =  function()
+    ["abyss"] = [[
         CMSG(" > Running Abyss Eye by Shackluster")
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Abyss%20Eye%20Convert.lua',true))()
-    end,
+    ]],
     
-    ["gale"] =  function()
+    ["gale"] = [[
         CMSG(" > Running Gale Fighter by makhail07 and KillerDarkness0105")
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Gale%20Fighter%20Convert.lua',true))()
-    end,
+    ]],
     
-    ["ender"] =  function()
+    ["ender"] = [[
         CMSG(" > Running Ender by makhail07")
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Ender%20Convert.lua',true))()
-    end,
+    ]],
     
-    ["sonic"] =  function()
+    ["sonic"] = [[
         CMSG(" > Running Sonic Script by KillerDarkness0105")
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Sonic%20The%20Hedehog%20Convert.lua',true))()
-    end,
+    ]],
     
-    ["memeus 2"] =  function()
+    ["memeus 2"] = [[
         CMSG(" > Running Memeus 2.0 by makhail07")
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Memeus%202.0%20Convert.lua',true))()
-    end,
+    ]],
     
-    ["memeus 2.5"] =  function()
+    ["memeus 2.5"] = [[
         CMSG(" > Running Memeus 2.5 originally by makhail07 edited by 2003boobear")
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Memeus%202.5%20Convert.lua',true))()
-    end,
+    ]],
     
-    ["chara"] =  function()
+    ["chara"] = [[
         CMSG(" > Running Chara by Nebula_Zorua")
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Chara%20Convert.lua',true))()
-    end,
+    ]],
     
-    ["sans"] =  function()
+    ["sans"] = [[
         CMSG(" > Running Undertale Sans by Nebula_Zorua")
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Undertale%20Sans%20Convert.lua',true))()
-    end,
+    ]],
     
-    ["caac"] =  function()
+    ["caac"] = [[
         CMSG(" > Running Jump In The CAAC By EthanHong0407")
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Jump%20In%20The%20CAAC%20Convert.lua',true))()
-    end,
+    ]],
     
-    ["ultima"] =  function()
+    ["ultima"] = [[
         CMSG(" > Running Dual Ultima by Sugarie Saffron")
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Dual%20Ultima%20Convert.lua',true))()
-    end,
+    ]],
     
-    ["server"] =  function()
+    ["server"] = [[
         CMSG(" > Running Server Admin by Shackluster")
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Server%20Admin%20Convert.lua',true))()
-    end,
+    ]],
     
-    ["meme"] =  function()
+    ["meme"] = [[
         CMSG(" > Running Meme Animations by 123jl123? [idk]")
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Meme%20Animations%20Convert.lua',true))()
-    end,
+    ]],
     
-    ["magic"] =  function()
+    ["magic"] = [[
         CMSG(" > Running Magic Unknown by KillerDarkness0105")
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Magic%20Unknown%20Convert.lua',true))()
-    end,
+    ]],
     
-    ["sassy"] =  function()
+    ["sassy"] = [[
         CMSG(" > Running Sassy Girl by Goodguyaiden")
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Sassy%20Girl%20Convert.lua',true))()
-    end,
+    ]],
 
-    ["darkheart"] =  function()
+    ["darkheart"] = [[
         CMSG(" > Running Linked Sword by SezHu")
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Linked%20Sword%20Convert.lua',true))()
-    end,
+    ]],
 
-    ["hover"] =  function()
+    ["hover"] = [[
         CMSG(" > Running Light HoverBoard by ???")
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Hoverboard%20Convert.lua',true))()
-    end,
+    ]],
 
-    ["ban"] =  function()
+    ["ban"] = [[
         CMSG(" > Running Ban Hammer by Creterisk")
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Ban%20Hammer%20Convert.lua',true))()
-    end,
+    ]],
 
-    ["chips"] =  function()
+    ["chips"] = [[
         CMSG(" > Running Chips by Creterisk")
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Chips%20Convert.lua',true))()
-    end,
+    ]],
 
-    ["chill"] = function()
+    ["chill"] = [[
         CMSG(" > Running Chill by ???")
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Chill%20Convert.lua',true))()
-    end,
+    ]],
 }
