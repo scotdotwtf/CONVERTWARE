cda36709a310aca6006a7d58c206457c585aec04
spec <77427029+specowos@users.noreply.github.com>
2021-12-25 20:56:07 -0600

    Update Nebula Star Glitcher Convert.lua

--- convertware/Scripts/Converted/Nebula Star Glitcher Convert.lua
+++ convertware/Scripts/Converted/Nebula Star Glitcher Convert.lua
@@ -1,8 +1,6 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/GelatekWasTaken/GelatekV2/main/LoadLibrary.lua',true))()
 print("> Running Nebula Star Glitcher by Nebula_Zorua")
 
-local hatstar = game:GetService("Players").LocalPlayer.Character['Northern Star']
-
 plr = Game:GetService("Players").LocalPlayer
 char = plr.Character.Dummy.Dummy
 hum = char.Humanoid
