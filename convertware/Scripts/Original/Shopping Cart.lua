7dabf63c23cbe46a0732a1d597abd156e6519d50
spec <77427029+specowos@users.noreply.github.com>
2022-04-15 21:52:04 -0500

    Create Shopping Cart.lua

---
+++ convertware/Scripts/Original/Shopping Cart.lua
@@ -0,0 +1,212 @@
+--By Rufus14
+mouse = game.Players.LocalPlayer:GetMouse()
+rig = game.Players.LocalPlayer.Character.Torso
+game.Players.LocalPlayer.Character.Animate:destroy()
+run = Instance.new("Sound", game.Players.LocalPlayer.Character.Head)
+run.SoundId = "rbxassetid://142665235"
+run.Volume = 0
+run.Looped = true
+run:Play()
+run.PlaybackSpeed = 1.2
+print(rig["Left Hip"].C0)
+print(rig["Right Hip"].C0)
+cart = Instance.new("Part", rig)
+cart.CanCollide = false
+weld = Instance.new("Weld", cart)
+weld.Part0 = cart
+weld.Part1 = rig
+mesh = Instance.new("SpecialMesh", cart)
+mesh.MeshId = "rbxassetid://552693518"
+mesh.TextureId = "rbxassetid://552693524"
+mesh.Scale = Vector3.new(7.2,7,8)
+weld.C0 = weld.C0 * CFrame.new(0,1,4.2)
+walking = true
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 25
+for i = 1,20 do
+    wait()
+    rig["Left Shoulder"].C0 = rig["Left Shoulder"].C0 * CFrame.Angles(0,0,-0.08)
+    rig["Right Shoulder"].C0 = rig["Right Shoulder"].C0 * CFrame.Angles(0,0,0.08)
+end
+function walk(key)
+    key = key:lower()
+    if key == "w" then
+    walking = true
+    run.Volume = 10
+    if walking == true then
+    wait()
+    for i = 1,5 do
+    rig["Left Hip"].C0 = rig["Left Hip"].C0 * CFrame.Angles(0,0,-0.2)
+    rig["Right Hip"].C0 = rig["Right Hip"].C0 * CFrame.Angles(0,0,-0.2)
+    end
+    while walking == true do
+    for i = 1,5 do
+    wait()
+    rig["Left Hip"].C0 = rig["Left Hip"].C0 * CFrame.Angles(0,0,0.4)
+    rig["Right Hip"].C0 = rig["Right Hip"].C0 * CFrame.Angles(0,0,0.4)
+    end
+    for i = 1,5 do
+    wait()
+    rig["Left Hip"].C0 = rig["Left Hip"].C0 * CFrame.Angles(0,0,-0.4)
+    rig["Right Hip"].C0 = rig["Right Hip"].C0 * CFrame.Angles(0,0,-0.4)
+    end
+    end
+    end
+    end
+    end
+mouse.KeyDown:connect(walk)
+function notwalk(key)
+    key = key:lower()
+    if key == "w" then
+        walking = false
+        run.Volume = 0
+        for i = 1,10 do
+        wait()
+        rig["Left Hip"].C0 = CFrame.new(-1, -1, 0, -0, -0, -1, 0, 1, 0, 1, 0, 0)
+        rig["Right Hip"].C0 = CFrame.new(1, -1, 0, 0, 0, 1, 0, 1, 0, -1, -0, -0)
+        end
+    end
+end
+mouse.KeyUp:connect(notwalk)
+---------------------------------
+walking1 = false
+function walk1(key)
+    key = key:lower()
+    if key == "d" then
+    walking1 = true
+    if walking1 == true then
+    wait()
+    run.Volume = 10
+    for i = 1,5 do
+    rig["Left Hip"].C0 = rig["Left Hip"].C0 * CFrame.Angles(0,0,-0.2)
+    rig["Right Hip"].C0 = rig["Right Hip"].C0 * CFrame.Angles(0,0,-0.2)
+    end
+    while walking1 == true do
+    for i = 1,5 do
+    wait()
+    rig["Left Hip"].C0 = rig["Left Hip"].C0 * CFrame.Angles(0,0,0.4)
+    rig["Right Hip"].C0 = rig["Right Hip"].C0 * CFrame.Angles(0,0,0.4)
+    end
+    for i = 1,5 do
+    wait()
+    rig["Left Hip"].C0 = rig["Left Hip"].C0 * CFrame.Angles(0,0,-0.4)
+    rig["Right Hip"].C0 = rig["Right Hip"].C0 * CFrame.Angles(0,0,-0.4)
+    end
+    end
+    end
+    end
+    end
+mouse.KeyDown:connect(walk1)
+function notwalk1(key)
+    key = key:lower()
+    if key == "d" then
+        walking1 = false
+        run.Volume = 0
+        for i = 1,10 do
+        wait()
+        rig["Left Hip"].C0 = CFrame.new(-1, -1, 0, -0, -0, -1, 0, 1, 0, 1, 0, 0)
+        rig["Right Hip"].C0 = CFrame.new(1, -1, 0, 0, 0, 1, 0, 1, 0, -1, -0, -0)
+        end
+    end
+end
+mouse.KeyUp:connect(notwalk1)
+------------------------------------------
+walking2 = false
+function walk2(key)
+    key = key:lower()
+    if key == "a" then
+    walking2 = true
+    run.Volume = 10
+    if walking2 == true then
+    wait()
+    for i = 1,5 do
+    rig["Left Hip"].C0 = rig["Left Hip"].C0 * CFrame.Angles(0,0,-0.2)
+    rig["Right Hip"].C0 = rig["Right Hip"].C0 * CFrame.Angles(0,0,-0.2)
+    end
+    while walking2 == true do
+    for i = 1,5 do
+    wait()
+    rig["Left Hip"].C0 = rig["Left Hip"].C0 * CFrame.Angles(0,0,0.4)
+    rig["Right Hip"].C0 = rig["Right Hip"].C0 * CFrame.Angles(0,0,0.4)
+    end
+    for i = 1,5 do
+    wait()
+    rig["Left Hip"].C0 = rig["Left Hip"].C0 * CFrame.Angles(0,0,-0.4)
+    rig["Right Hip"].C0 = rig["Right Hip"].C0 * CFrame.Angles(0,0,-0.4)
+    end
+    end
+    end
+    end
+    end
+mouse.KeyDown:connect(walk2)
+function notwalk2(key)
+    key = key:lower()
+    if key == "a" then
+        walking2 = false
+        run.Volume = 0
+        for i = 1,10 do
+        wait()
+        rig["Left Hip"].C0 = CFrame.new(-1, -1, 0, -0, -0, -1, 0, 1, 0, 1, 0, 0)
+        rig["Right Hip"].C0 = CFrame.new(1, -1, 0, 0, 0, 1, 0, 1, 0, -1, -0, -0)
+        end
+    end
+end
+mouse.KeyUp:connect(notwalk2)
+----------------------------------------------------
+walking3 = false
+function walk3(key)
+    key = key:lower()
+    if key == "s" then
+    walking3 = true
+    run.Volume = 10
+    if walking3 == true then
+    wait()
+    for i = 1,5 do
+    rig["Left Hip"].C0 = rig["Left Hip"].C0 * CFrame.Angles(0,0,-0.2)
+    rig["Right Hip"].C0 = rig["Right Hip"].C0 * CFrame.Angles(0,0,-0.2)
+    end
+    while walking3 == true do
+    for i = 1,5 do
+    wait()
+    rig["Left Hip"].C0 = rig["Left Hip"].C0 * CFrame.Angles(0,0,0.4)
+    rig["Right Hip"].C0 = rig["Right Hip"].C0 * CFrame.Angles(0,0,0.4)
+    end
+    for i = 1,5 do
+    wait()
+    rig["Left Hip"].C0 = rig["Left Hip"].C0 * CFrame.Angles(0,0,-0.4)
+    rig["Right Hip"].C0 = rig["Right Hip"].C0 * CFrame.Angles(0,0,-0.4)
+    end
+    end
+    end
+    end
+    end
+mouse.KeyDown:connect(walk3)
+function notwalk3(key)
+    key = key:lower()
+    if key == "s" then
+        walking3 = false
+        run.Volume = 0
+        for i = 1,10 do
+        wait()
+        rig["Left Hip"].C0 = CFrame.new(-1, -1, 0, -0, -0, -1, 0, 1, 0, 1, 0, 0)
+        rig["Right Hip"].C0 = CFrame.new(1, -1, 0, 0, 0, 1, 0, 1, 0, -1, -0, -0)
+        end
+    end
+end
+mouse.KeyUp:connect(notwalk3)
+grabbing = true
+function grab1(cart1)
+        if grabbing == true then
+        local torso = cart1.Parent:findFirstChild("Torso")
+                if torso ~= nil then
+                local hum = cart1.Parent:findFirstChild("Humanoid")
+                if hum ~= nil then
+                hum.PlatformStand = true
+        local weld1 = Instance.new("Weld", torso)
+        weld1.Part0 = torso
+        weld1.Part1 = cart
+        weld1.C0 = weld1.C0 * CFrame.Angles(-8,0,1.2) * CFrame.new(0,-1.4,0)
+                end
+                end
+    end
+    end
+cart.Touched:connect(grab1)
