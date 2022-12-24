c22687174dc3fcc05559f59b2faefaa7f417f5ce
spec <77427029+specowos@users.noreply.github.com>
2022-12-23 18:55:57 -0600

    Usless so im deleting

--- Load.lua
+++
@@ -1,2 +0,0 @@
---// Enjoy! Join the discord for info on how to use https://discord.gg/eTTjPKBdfT
-loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/UI/main.lua',true))()
